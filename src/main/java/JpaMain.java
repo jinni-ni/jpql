9173b0cbb0ac18c26bcf3eae10adfe13b37dbf76
김수진 <sjkim910115@gmail.com>
2020-05-17 13:28:54 +0900

    Initial commit

---
+++ src/main/java/JpaMain.java
@@ -0,0 +1,42 @@
+import jpql.Member;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m",Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m",String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+
+            TypedQuery<Member> query = em.createQuery("select m from Member m",Member.class);
+            List<Member> resultList = query.getResultList();
+
+            TypedQuery<Member> query5 = em.createQuery("select m from Member m where m.id= 10L",Member.class);
+            // getSingleReuslt()
+            // 결과 0, 두개 이상 exception 발생
+            Member result = query.getSingleResult();
+            System.out.println("reuslt = "+result);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
+
